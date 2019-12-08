30fc178b7dcccbd53f39e58553ad7ee8ef99022c
achen <zhou_gc@suixingpay.com>
2019-12-08 20:34:53 +0800

    Initial commit

---
+++ server/src/main/java/com/xinghuofirst/kill/server/MainApplication.java
@@ -0,0 +1,18 @@
+package com.xinghuofirst.kill.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @Author:zhou_gc
+ * @Date: 2019/12/8 22:50
+ **/
+@SpringBootApplication
+public class MainApplication   {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+
+}
