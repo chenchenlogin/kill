b0757b5582d89496935ce71e777b027b36086bf4
dupeng <1710496709@qq.com>
2019-12-09 21:40:59 +0800

    添加kill的控制层

--- server/src/main/java/com/xinghuofirst/kill/server/controller/KillSuccessController.java
+++ server/src/main/java/com/xinghuofirst/kill/server/controller/KillSuccessController.java
@@ -1,26 +1,15 @@
-/**
- * All rights Reserved, Designed By Suixingpay.
- *
- * @author: zhangleying<zhang_yy2@suixingpay.com>
- * @date: 2019/12/08 17:25
- * @Copyright: 2019 Suixingpay. All rights reserved.
- * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
- */
 package com.xinghuofirst.kill.server.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @description:
- * @author: zhangleying<zhang_yy2@suixingpay.com>
- * @date: 2019/12/08 17:25
+ * @description: TODO
+ * @author: 杜鹏
+ * @date: 2019-12-09 21:39
  * @version: V1.0
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping
 public class KillSuccessController {
-
-
 }
