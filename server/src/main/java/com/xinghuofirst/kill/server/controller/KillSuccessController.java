dd24c10a7c15eb4a771c02bdf9357b299ea5305f
js20166098 <56857589+js20166098@users.noreply.github.com>
2019-12-11 15:04:11 +0800

    Update KillSuccessController.java

--- server/src/main/java/com/xinghuofirst/kill/server/controller/KillSuccessController.java
+++ server/src/main/java/com/xinghuofirst/kill/server/controller/KillSuccessController.java
@@ -1,10 +1,6 @@
 /**
- * All rights Reserved, Designed By Suixingpay.
- *
- * @author: zhangleying<zhang_yy2@suixingpay.com>
+ * @author:姜爽
  * @date: 2019/12/08 17:25
- * @Copyright: 2019 Suixingpay. All rights reserved.
- * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
  */
 package com.xinghuofirst.kill.server.controller;
 
@@ -27,7 +23,7 @@ import java.util.Map;
 
 /**
  * @description:
- * @author: zhangleying<zhang_yy2@suixingpay.com>
+ * @author: 姜爽
  * @date: 2019/12/08 17:25
  * @version: V1.0
  */
